98312600f78021d7d7fd57588eda3a70159ce9b3
makiki99 <mat.gawro@gmail.com>
2016-10-16 10:31:19 +0200

    made level loader

--- code/main.js
+++ code/main.js
@@ -2,11 +2,12 @@ const TSIZE = 32;
 let assets = [
 	"img/playerL.png",
 	"img/playerR.png",
-	"img/tileset.png"
+	"img/tileset.png",
+	"img/goal.png"
 ];
 let keys = [];
-let g = hexi(TSIZE*16,TSIZE*16, setup, assets, load);
-let scene, player, walls, bgElements, gamefield;
+let g = hexi(TSIZE*16,TSIZE*17, setup, assets, load);
+let scene, player, walls, bgElements, gamefield, goal;
 
 g.start();
 
@@ -16,7 +17,8 @@ function load() {
 
 function setup() {
 	scene = g.group();
-	gamefield = g.rectangle(TSIZE*16, TSIZE*16, "black");
+	gamefield = g.rectangle(TSIZE*16, TSIZE*16, 0x000000);
+	gamefield.alpha = 0;
 	gamefield.x = 0;
 	gamefield.y = 0;
 	scene.addChild(gamefield);
@@ -24,6 +26,8 @@ function setup() {
 	scene.addChild(walls);
 	bgElements = g.group();
 	scene.addChild(bgElements);
+	goal = g.sprite("img/goal.png");
+	scene.addChild(goal);
 	loadPlayer();
 	loadLevel(sampleLevel);
 	g.state = play;
